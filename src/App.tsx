1c6d25a7eb8ca5fe3cb433c9aa125575d39f0918
SlatinskyJ <jakub.slatinsky@gmail.com>
2022-11-28 15:16:21 +0100

    Fixed code issues, added react query, zod and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import Todo from './Todo';
-
-// rework this into regular api call, feel free to use any open api
-var todos = (): Promise<{id: string; title: string;}[]> => new Promise((res) => {
-	setTimeout(() => {
-		res([
-			{
-				id: "1",
-				title: "Go shopping",
-			},
-			{
-				id: "2",
-				title: "Job interview",
-			},
-			{
-				id: "3",
-				title: "Prepare homework",
-			},
-		]);
-	}, 100);
-});
+import Todo from './components/atoms/Todo';
+import {useGetAllTodos} from './components/model/Todo/getAllTodos';
 
 function App() {
-	const [state, setState] = React.useState<{ id: string; title: string }[]>([]);
+	const {
+		data, isLoading, error
+	} = useGetAllTodos();
 
-	React.useEffect(() => {
-		(async () => {
-			var awaitedTodos = await todos();
-			for (var i = 0; i < awaitedTodos.length; i++) {
-				setState([...state, awaitedTodos[i]]);
-			}
-		})()
-	})
+	if (isLoading || error) return <div>Loading!</div>;
 
 	return (
 		<div>
-			{state.map((todo) => (
-				<Todo todo={todo} />
+			{data?.map((todo) => (
+				<Todo todo={todo} key={todo.id}/>
 			))}
 		</div>
 	);
---
+++ src/components/model/Todo/getAllTodos.ts
@@ -0,0 +1,33 @@
+import {useQuery} from '@tanstack/react-query';
+import {APIFetchType} from '../commonTypes';
+import {TodoListType, TodoListTypeValidator} from './Todo';
+
+export function useGetAllTodos(): APIFetchType<TodoListType> {
+	const {data, isLoading, error} = useQuery(['getAllTodos'], () => todos());
+
+	return {
+		data: data ? TodoListTypeValidator.parse(data) : undefined,
+		isLoading,
+		error
+	};
+}
+
+// rework this into regular api call, feel free to use any open api
+export const todos = (): Promise<unknown> => new Promise((res) => {
+	setTimeout(() => {
+		res([
+			{
+				id: '1',
+				title: 'Go shopping',
+			},
+			{
+				id: '2',
+				title: 'Job interview',
+			},
+			{
+				id: '3',
+				title: 'Prepare homework',
+			},
+		]);
+	}, 1000);
+});
